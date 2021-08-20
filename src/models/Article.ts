a29ef02431d305eb803aa4dc9143cdbc2811d054
GustavoGomesDias <treeelevengg@gmail.com>
2021-08-20 02:47:52 -0300

    feat(models): add user and article models

---
+++ src/models/Article.ts
@@ -0,0 +1,27 @@
+global.db?.createCollection('articles', {
+  validator: {
+    $jsonScheme: {
+      bsonType: 'object',
+      required: ['title', 'description', 'content', 'user_id'],
+      properties: {
+        title: {
+          bsonType: 'string',
+          description: 'must be a string and is required',
+        },
+        description: {
+          bsonType: 'string',
+          description: 'must be a string and is required',
+        },
+        content: {
+          bsonType: 'string',
+          description: 'must be a string and is required',
+        },
+        user_id: {
+          $ref: 'users',
+          $id: 'objectId',
+          $db: 'blog',
+        },
+      },
+    },
+  },
+});
